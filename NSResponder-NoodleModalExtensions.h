7630bc7920037ce40d3a581d42e701c8c07346fd
Paul Kim <mr_noodle@noodlesoft.com>
2010-07-01 14:35:43 -0400

    Fixed license agreement that was either missing or had typos.

--- NSResponder-NoodleModalExtensions.h
+++ NSResponder-NoodleModalExtensions.h
@@ -5,12 +5,11 @@
 //  Created by Paul Kim on 3/6/08.
 //  Copyright 2008-2009 Noodlesoft, LLC. All rights reserved.
 //
-//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
-// nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// ncopies of the Software, and to permit persons to whom the Software is
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in
